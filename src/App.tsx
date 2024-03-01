2a5806c1219ae398d795ab4a44ee72d9c065f787
Nick Grato <nickgrato@ngrato-27545.local>
2024-02-29 20:19:57 -0800

    Creating some layouts configurations and folder structure. Putting a few shade examples

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { ProjectsApi } from "./api/apis/ProjectsApi"; // Add this line
 import { Configuration } from "./api";
 import { Button } from "@/components/ui/button";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
+import { GlobalLayout } from "@/components/modules/layouts/GlobalLayout/GlobalLayout";
 
 function App() {
   const projectsApi = new ProjectsApi(
-    new Configuration({ basePath: "http://localhost:4444" }),
+    new Configuration({ basePath: "http://localhost:4444" })
   );
 
   const handleButtonClick = async () => {
@@ -24,27 +26,25 @@ function App() {
   };
 
   return (
-    <>
-      <button
-        className="text-3xl font-bold underline"
-        onClick={handleButtonClick}
-      >
-        List Projects
-      </button>
-
-      <Button variant="outline" onClick={handleButtonClick}>
-        Another Button
-      </Button>
-
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </>
+    // Routing needs to be set up here.
+    <GlobalLayout>
+      <main className="p-12">
+        <Button onClick={handleButtonClick}>List Projects</Button>
+        <h2 className="mt-12 mb-4">context form</h2>
+        <ContextMenu>
+          <ContextMenuTrigger>
+            {" "}
+            <Button>Right click to see context menu</Button>
+          </ContextMenuTrigger>
+          <ContextMenuContent>
+            <ContextMenuItem>Profile</ContextMenuItem>
+            <ContextMenuItem>Billing</ContextMenuItem>
+            <ContextMenuItem>Team</ContextMenuItem>
+            <ContextMenuItem>Subscription</ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
+      </main>
+    </GlobalLayout>
   );
 }
 
