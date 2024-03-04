b77004d24e15e37b20f020ccd7057da1cca7ac22
John Shaughnessy <john@johnshaughnessy.com>
2024-03-04 12:10:09 -0500

    Generate api client files

---
+++ client3/src/api/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './FilesApi';
+export * from './IngestApi';
+export * from './LlamafileApi';
+export * from './ProjectsApi';
+export * from './RagApi';
+export * from './SummariesApi';
